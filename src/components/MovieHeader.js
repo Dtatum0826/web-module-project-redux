b9a9bf59d0ab1133310d65bed03e3a66356d7e69
Dustin Tatum <dtatum0826@gmail.com>
2023-03-30 18:31:37 -0500

    Completed Movie reducer

--- src/components/MovieHeader.js
+++ src/components/MovieHeader.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {connect}from 'react-redux'
+
 
 const MovieHeader = (props) => {
-    const appTitle = "";
+    const appTitle = props.appTitle;
     const displayFavorites = true;
+
+
     
     return(<div className="table-title">
         <div className="row">
@@ -13,10 +17,15 @@ const MovieHeader = (props) => {
         <div className="col-sm-6 headerBar">
             <div className="btn btn-sm btn-primary"><span>{ displayFavorites ? "Hide" : "Show"} Favorites</span></div>
             <Link to="/movies" className="btn btn-sm btn-primary">View All Movies</Link>
-            <Link to="/movies/add" className="btn btn-sm btn-success"><i className="material-icons">&#xE147;</i> <span>Add New Movie</span></Link>
+            <Link to="/movies/add" className="btn btn-sm btn-success"><i className="material-icons">&#xE147;</i> <span >Add New Movie</span></Link>
         </div>
         </div>
     </div>);
 }
-
-export default MovieHeader;
+const mapStateToProps = (state) =>{
+    return({
+        appTitle: state.appTitle,
+        
+    })
+}
+export default connect(mapStateToProps,{})(MovieHeader);
